81adcf70717222ed1a61d14bac68e6ae08e34c17
Ricky Chon <rickychon99@gmail.com>
2021-06-06 16:16:18 -0400

    Remove trailing whitespace

--- week1/assignments/reverse-only-letters/reverse_only_letters.py
+++ week1/assignments/reverse-only-letters/reverse_only_letters.py
@@ -2,21 +2,21 @@ class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
         """
         Returns the reversed string, where any character that's not a letter stays in the same place.
-        
+
         constraints:
             no more than 100 characters, could have empty string
             char ASCII codes are between 33 and 122 inclusive
             string does not contain \ or "
-            
+
         example:
             input: s = "abc-d"
             output: "dcb-a"
             
             input: s = "Leet-code!"
             output: "edoc-teeL!"
-            
+
         pseudocode:
-        
+
             def reverseOnlyLetters(s):
                 pointer on both ends of string
                 iterate through string:
@@ -25,7 +25,7 @@ class Solution:
         """
         start, end = 0, len(s) - 1
         new_s = list(s)
-        
+
         while start < end:
             if not s[start].isalpha():
                 start += 1
@@ -35,5 +35,5 @@ class Solution:
                 new_s[start], new_s[end] = new_s[end], new_s[start]
                 start += 1
                 end -= 1
-        
+
         return ''.join(new_s)
